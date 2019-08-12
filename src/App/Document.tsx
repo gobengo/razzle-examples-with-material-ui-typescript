87ca5deb78abdb988aa119844c352a6556439bbc
Benjamin Goering <ben@bengo.co>
2019-08-12 12:00:23 +0400

    Add after.js

---
+++ src/App/Document.tsx
@@ -0,0 +1,59 @@
+import { AfterData, AfterRoot } from "@jaredpalmer/after";
+import { ServerStyleSheets, ThemeProvider } from "@material-ui/styles";
+import React, { Component } from "react";
+import theme from "./theme";
+
+export class Document extends Component<any, any> {
+  public static async getInitialProps({ assets, data, renderPage }: any) {
+    const sheets = new ServerStyleSheets();
+
+    const page = await renderPage(
+      (App: React.ComponentType<any>) => (props: any) =>
+        sheets.collect(
+          <ThemeProvider theme={theme}>
+            <App {...props} />
+          </ThemeProvider>,
+        ),
+    );
+
+    return { assets, data, sheets, ...page };
+  }
+
+  public render() {
+    const { helmet, assets, data, sheets } = this.props; // 4) Get sheets from props
+
+    // get attributes from React Helmet
+    const htmlAttrs = helmet.htmlAttributes.toComponent();
+    const bodyAttrs = helmet.bodyAttributes.toComponent();
+    const assetStylesheetLink = assets.client.css && (
+      <link rel="stylesheet" href={assets.client.css} />
+    );
+    return (
+      <html {...htmlAttrs}>
+        <head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta charSet="utf-8" />
+          <title>Razzle</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {helmet.title.toComponent()}
+          {helmet.meta.toComponent()}
+          {helmet.link.toComponent()}
+          {assetStylesheetLink}
+          <style type="text/css">{sheets.toString()}</style>
+        </head>
+        <body {...bodyAttrs}>
+          <AfterRoot />
+          <AfterData data={data} />
+          <script
+            type="text/javascript"
+            src={assets.client.js}
+            defer={true}
+            crossOrigin="anonymous"
+          />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default Document;
--- src/client.tsx
+++ src/client.tsx
@@ -1,22 +1,28 @@
+import { After, ensureReady } from "@jaredpalmer/after";
 import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import { hydrate } from "react-dom";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import routes from "./routes";
 import theme from "./theme";
 
-hydrate(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById("root"),
-  () => {
-    // [ReHydratation](https://github.com/cssinjs/jss/blob/master/docs/ssr.md)
-    const jssStyles = document.getElementById("jss-ssr");
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-  },
-);
+ensureReady(routes).then(data => {
+  hydrate(
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <After data={data} routes={routes} />
+      </ThemeProvider>
+    </BrowserRouter>,
+    document.getElementById("root"),
+    () => {
+      // [ReHydratation](https://github.com/cssinjs/jss/blob/master/docs/ssr.md)
+      const jssStyles = document.getElementById("jss-ssr");
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles);
+      }
+    },
+  );
+});
 
 if (module.hot) {
   module.hot.accept();
